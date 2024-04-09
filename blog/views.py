4c0ad06073f2d5adb56caf8d06eb720a91d10166
Giulio Montaque <giuliom@hotmail.co.uk>
2024-04-09 16:05:42 +0000

    addded in filter

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,8 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all().order_by("created_on")
+    # queryset = Post.objects.all()
+    # model = Post
     template_name = "post_list.html"
