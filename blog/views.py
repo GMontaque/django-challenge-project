c28e5005725abd330829eb16c371a7d68fa2303d
Giulio Montaque <giuliom@hotmail.co.uk>
2024-04-11 16:25:24 +0000

    added template

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,10 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.filter(status=1)
     # queryset = Post.objects.all().order_by("created_on")
-    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
     # model = Post
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
