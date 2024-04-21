9c389cc179b1b222f24c005e8d859b890308f0c8
Giulio Montaque <giuliom@hotmail.co.uk>
2024-04-21 11:15:27 +0000

    add comments and docstring to code

--- about/views.py
+++ about/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from .models import About
-from django.shortcuts import render, get_object_or_404
-from .forms import CollaborateForm
 from django.contrib import messages
+from .forms import CollaborateForm
+from .models import About
 
 # Create your views here.
 def about_detail(request):
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
+    """
+
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
@@ -29,17 +44,3 @@ def about_detail(request):
     )
 
 
-#  if request.method == "POST":
-#         print("Received a POST request")
-#         comment_form = CommentForm(data=request.POST)
-#         if comment_form.is_valid():
-#             comment = comment_form.save(commit=False)
-#             comment.author = request.user
-#             comment.post = post
-#             comment.save()
-#             messages.add_message(
-#                 request, messages.SUCCESS,
-#                 'Comment submitted and awaiting approval'
-#             )   
-
-#     comment_form = CommentForm()
